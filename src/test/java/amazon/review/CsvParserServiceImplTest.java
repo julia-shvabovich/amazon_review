8295f461345dbe0f3352738c2ea6000161b4c901
jlightgreen <ShvabovichJulia@gmail.com>
2020-11-12 15:43:25 +0300

    CSV file parsing implementation

---
+++ src/test/java/amazon/review/CsvParserServiceImplTest.java
@@ -0,0 +1,52 @@
+package amazon.review;
+
+import amazon.review.dto.ReviewDto;
+import amazon.review.service.CsvReaderService;
+import amazon.review.service.impl.CsvParserServiceImpl;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+public class CsvParserServiceImplTest {
+    private static final String FILE_NAME = "src/main/resources/one_line.csv";
+    private static final String WRONG_FORMAT = "src/main/resources/wrong_format.csv";
+
+    @Autowired
+    private CsvReaderService readerService;
+
+    @Test
+    public void parseOkLine() {
+        ReviewDto expectedDto = new ReviewDto();
+        expectedDto.setId(1L);
+        expectedDto.setProductId("B001E4KFG0");
+        expectedDto.setUserId("A3SGXH7AUHU8GW");
+        expectedDto.setProfileName("delmartian");
+        expectedDto.setHelpfulnessNumerator(1);
+        expectedDto.setHelpfulnessDenominator(1);
+        expectedDto.setScore(5);
+        expectedDto.setTime(1303862400L);
+        expectedDto.setSummary("Good Quality Dog Food");
+        expectedDto.setText("I have bought several of the Vitality canned dog food products" +
+                " and have found them all to be of good quality.");
+        String line = readerService.read(FILE_NAME).get(0);
+        Assert.assertEquals(expectedDto,
+                new CsvParserServiceImpl().parseLine(line));
+    }
+
+    @Test
+    public void parseWrongFormatLine() {
+        assertThrows(RuntimeException.class, () -> {
+            new CsvParserServiceImpl().parseLine(readerService.read(WRONG_FORMAT).get(0));
+        });
+    }
+
+    @Test
+    public void parseEmptyLine() {
+        assertThrows(RuntimeException.class, () -> {
+            new CsvParserServiceImpl().parseLine(null);
+        });
+    }
+}
--- src/test/java/amazon/review/CsvReaderServiceImplTest.java
+++ src/test/java/amazon/review/CsvReaderServiceImplTest.java
@@ -1,11 +1,11 @@
 package amazon.review;
 
 import amazon.review.service.CsvReaderService;
+import java.util.List;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
